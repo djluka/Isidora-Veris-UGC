fed7938f7a85a1fba5740d9fcfa45f280f57830e
djluka <66550706+djluka@users.noreply.github.com>
2025-08-03 18:41:46 +0200

    Update rasults.js

--- scripts/rasults.js
+++ scripts/rasults.js
@@ -9,22 +9,19 @@ function results() {
     logo.addEventListener("click", () => {
       const selectedValue = logo.getAttribute("value");
 
-      // Sakrij sve komentare
       comments.forEach((comment) => {
         comment.style.display = "none";
       });
 
-      // Prikazi samo odgovarajuci komentar
       if (selectedValue) {
         const target = document.querySelector(`.comment[value="${selectedValue}"]`);
         if (target) {
-          target.style.display = "flex"; // ili "block" ako ne koristiš flexbox
+          target.style.display = "flex";
         }
       }
     });
   });
 
-  // Možeš dodati i da na početku sakriješ sve komentare
   comments.forEach((comment) => {
     comment.style.display = "none";
   });
