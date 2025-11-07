a3627affc7f0e89131d478c93e43b555d96f1cf7
djluka <66550706+djluka@users.noreply.github.com>
2025-11-07 02:21:54 +0100

    Refactor "about me" script: simplify video logic and remove redundant code

--- public/scripts/about_me.js
+++ public/scripts/about_me.js
@@ -6,23 +6,19 @@ function loop() {
     const video = document.querySelector(".about_me .about_me_content video.profile_loop_video");
     const videoSrcMobile = video.dataset.src_mobile;
     const videoSrcDesktop = video.dataset.src_desktop;
+    console.log(video);
 
-    window.addEventListener("resize", function () {
-        console.log("Promenjena je veličina prozora!");
-
+    function logic(){
         if (window.innerWidth < 1400) {
             video.src = videoSrcMobile;
-            console.log(video);
-
         } else {
             video.src = videoSrcDesktop;
-            console.log(video);
+        }
+    }
+    logic()
 
-        }
-        if (video.src) {
-            video.load();
-            video.play()
-        }
+    window.addEventListener("resize", function () {
+        logic()
     });
 
 
