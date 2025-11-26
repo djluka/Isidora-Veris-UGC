38ed54a24dd3e68aedc9c1193a728f89c3db7ea7
djluka <66550706+djluka@users.noreply.github.com>
2025-11-26 03:22:44 +0100

    Enhance footer animations with SVG elements, update styles for smoother transitions, and improve layout responsiveness.

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,10 +1,29 @@
 const footerMobile = document.querySelector('.footer_mobile');
-console.log(footerMobile);
+const arrowBody = document.querySelector('#arrowBodyMobile');
+const drawHead1 = document.querySelector('#arrowHead1Mobile');
+const drawHead2 = document.querySelector('#arrowHead2Mobile');
+const mailIconFooter = document.querySelector('#mailIconFooterMobile');
+
+// const arrowBodyDesktop = document.querySelector('#arrowBodyDesktop');
+// const drawHead1Desktop = document.querySelector('#arrowHead1Desktop');
+// const drawHead2Desktop = document.querySelector('#arrowHead2Desktop');
+// const mailIconFooterDesktop = document.querySelector('#mailIconFooterDesktop');
+
 const io = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            footerMobile.classList.add('animate-footer');
 
+            footerMobile.classList.add('animate-footer');
+            arrowBody.classList.add('animate-arrow');
+            drawHead1.classList.add('animate-arrow-head');
+            drawHead2.classList.add('animate-arrow-head');
+            mailIconFooter.classList.add('animate_footer_img');
+
+
+            // arrowBodyDesktop.classList.add('animate-arrow');
+            // drawHead1Desktop.classList.add('animate-arrow-head');
+            // drawHead2Desktop.classList.add('animate-arrow-head');
+            // mailIconFooterDesktop.classList.add('animate_footer_img');
         }
     });
 }, {threshold: 0.2});
