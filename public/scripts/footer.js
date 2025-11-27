a1081d17677b6bed56e8c6b999972770e2bf4313
djluka <66550706+djluka@users.noreply.github.com>
2025-11-27 01:54:15 +0100

    Refactor footer animation logic to support desktop, update styles for enhanced responsiveness, and improve SVG animations and transitions.

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,13 +1,15 @@
+const footer = document.querySelector('footer');
+
 const footerMobile = document.querySelector('.footer_mobile');
 const arrowBody = document.querySelector('#arrowBodyMobile');
 const drawHead1 = document.querySelector('#arrowHead1Mobile');
 const drawHead2 = document.querySelector('#arrowHead2Mobile');
 const mailIconFooter = document.querySelector('#mailIconFooterMobile');
 
-// const arrowBodyDesktop = document.querySelector('#arrowBodyDesktop');
-// const drawHead1Desktop = document.querySelector('#arrowHead1Desktop');
-// const drawHead2Desktop = document.querySelector('#arrowHead2Desktop');
-// const mailIconFooterDesktop = document.querySelector('#mailIconFooterDesktop');
+const arrowBodyDesktop = document.querySelector('#arrowBodyDesktop');
+const drawHead1Desktop = document.querySelector('#arrowHead1Desktop');
+const drawHead2Desktop = document.querySelector('#arrowHead2Desktop');
+const mailIconFooterDesktop = document.querySelector('#mailIconFooterDesktop');
 
 const io = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -20,12 +22,12 @@ const io = new IntersectionObserver((entries) => {
             mailIconFooter.classList.add('animate_footer_img');
 
 
-            // arrowBodyDesktop.classList.add('animate-arrow');
-            // drawHead1Desktop.classList.add('animate-arrow-head');
-            // drawHead2Desktop.classList.add('animate-arrow-head');
-            // mailIconFooterDesktop.classList.add('animate_footer_img');
+            arrowBodyDesktop.classList.add('animate-arrow-desktop');
+            drawHead1Desktop.classList.add('animate-arrow-head-desktop');
+            drawHead2Desktop.classList.add('animate-arrow-head-desktop');
+            mailIconFooterDesktop.classList.add('animate_footer_img-desktop');
         }
     });
 }, {threshold: 0.2});
 
-io.observe(footerMobile);
+io.observe(footer);
